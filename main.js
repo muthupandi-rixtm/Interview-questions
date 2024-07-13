68b959f8983bd1b13f07306459891eaae4ddbf33
Muthupandi2002 <muthupandi1112002@gmail.com>
2024-07-13 21:15:41 +0530

    addDaystoDate

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Check if two dates are equal 07/12/2024 and 12-Jun-2024
+
 function CompareDates() {
   let date1 = new Date("07/12/2024");
   let date2 = new Date("12-Jun-2024");
@@ -16,3 +18,19 @@ function CompareDates() {
 
   console.log(result);
 }
+
+//Add n days to an existing date ex - add 5 days to any given date
+
+function addDaystoDate(date, days) {
+  let result = new Date(date);
+
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+let originalDate = new Date("2024-07-10");
+let daysToAdd = 5;
+let newDate = addDaystoDate(originalDate, daysToAdd);
+
+console.log(`Original Date : ${originalDate.toDateString()}`);
+console.log(`New Date :${newDate.toDateString()}`);
